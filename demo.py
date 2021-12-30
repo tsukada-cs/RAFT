0374f57a33dccd2f81418ce600cb0815b6133030
tsukada-cs <tsukada.cs@gmail.com>
2021-12-31 06:52:21 +0900

    CPUモードに切り替えを修正

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ from utils.utils import InputPadder
 
 
 
-DEVICE = 'cuda'
+DEVICE = 'cpu'
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
@@ -42,7 +42,7 @@ def viz(img, flo):
 def demo(args):
     model = torch.nn.DataParallel(RAFT(args))
     # model.load_state_dict(torch.load(args.model))
-    model.load_state_dict(torch.load(args.model), map_location=torch.device('cpu')) 
+    model.load_state_dict(torch.load(args.model, map_location=torch.device('cpu'))) 
 
     model = model.module
     model.to(DEVICE)
