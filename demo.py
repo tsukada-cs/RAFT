d96eb8cbfeb002ab43da6db8655054faf8b2bd93
tsukada-cs <tsukada.cs@gmail.com>
2021-12-31 07:55:31 +0900

    alpha 対応など

--- demo.py
+++ demo.py
@@ -19,24 +19,31 @@ DEVICE = 'cpu'
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
+    if img.shape[-1] >= 4:
+        img = img[:,:,:3] # delete alpha cnannel
     img = torch.from_numpy(img).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
 
 
-def viz(img, flo):
+def viz(img, flo, fname="flow.png"):
     img = img[0].permute(1,2,0).cpu().numpy()
     flo = flo[0].permute(1,2,0).cpu().numpy()
     
     # map flow to rgb image
     flo = flow_viz.flow_to_image(flo)
-    img_flo = np.concatenate([img, flo], axis=0)
+    # img_flo = np.concatenate([img, flo], axis=1)
 
-    # import matplotlib.pyplot as plt
-    # plt.imshow(img_flo / 255.0)
-    # plt.show()
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    ax[0].imshow(img / 255.0)
+    ax[1].imshow(flo / 255.0)
+    ax[0].axis("off")
+    ax[1].axis("off")
+    fig.savefig(fname, bbox_inches="tight", pad_inches=0.1, dpi=144,)
+    plt.close()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    # cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    # cv2.waitKey()
 
 
 def demo(args):
@@ -61,7 +68,7 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+            viz(image1, flow_up, fname="demo-results/flo_"+os.path.basename(imfile1)+".png")
 
 
 if __name__ == '__main__':
@@ -72,5 +79,5 @@ if __name__ == '__main__':
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
     args = parser.parse_args()
-
+    print(args)
     demo(args)
